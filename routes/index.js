dbf5ca7995fd75eeae28ae6cad0c4487fe4544fa
i-danunzio <idanunzio@gmail.com>
2019-11-26 00:46:48 -0300

    Commit de Emilia dificil

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+const indexController = require('../controller/indexController');
+
+/* GET home page. */
+router.get('/', indexController.root);
+
+module.exports = router;
